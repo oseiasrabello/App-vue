70b44ec0d8080e1382564aa6f61ad997d507356c
ozeias gomes <oseiasrabelo911@gmail.com>
2022-06-08 18:17:42 -0300

    MInha primeira pagina com o Vue js

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <NavbarComponents />
+  <FooterComponents />
+</template>
+
+<script>
+import FooterComponents from "./components/Footer/FooterComponents.vue";
+import NavbarComponents from "./components/Menu/NavbarComponents.vue";
+
+export default {
+  name: "App",
+  components: {
+    NavbarComponents,
+    FooterComponents,
+  },
+};
+</script>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100vh;
+  background-color: aliceblue;
+}
+</style>
---
+++ src/components/Menu/NavbarComponents.vue
@@ -0,0 +1,52 @@
+<template>
+  <header class="container_header">
+    <nav class="navbar">
+      <legend class="logo">LOGO</legend>
+      <div class="menu">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/">About</a></li>
+          <li><a href="/">Contacts</a></li>
+        </ul>
+      </div>
+      <label for="">Menu</label>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "NavbarComponents",
+};
+</script>
+
+<style scoped>
+.container_header {
+  width: 100%;
+}
+
+.navbar {
+  width: 100%;
+  height: 60px;
+  background-color: #222;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.menu ul {
+  display: flex;
+  gap: 30px;
+}
+
+.menu ul li {
+  list-style-type: none;
+}
+
+.menu ul li a {
+  color: #ffff;
+  text-decoration: none;
+  font-size: 17px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
